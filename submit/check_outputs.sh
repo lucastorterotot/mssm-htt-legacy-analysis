55ded93c74af6afc0e5e7ec23a6bb9c5d5b833ed
Maximilian Burkart <maxiburkart@gmail.com>
2021-01-07 15:03:29 +0100

    Allow checks of control plot shape completeness in shell script.

--- submit/check_outputs.sh
+++ submit/check_outputs.sh
@@ -3,8 +3,16 @@
 ERA=$1
 IFS="," read -r -a CHANNELS <<< $2
 TAG=$3
+CONTROL=$4
+
+[[ ! -z $4 ]] || CONTROL=0
+CONTROL_ARG=""
+if [[ $CONTROL == 1 ]]
+then
+    CONTROL_ARG="--control"
+fi
 
 source utils/setup_root.sh
 source utils/setup_susy_samples.sh $ERA
 
-python submit/check_outputs.py -e $ERA -c ${CHANNELS[@]} --tag $TAG
+python submit/check_outputs.py -e $ERA -c ${CHANNELS[@]} --tag $TAG $CONTROL_ARG

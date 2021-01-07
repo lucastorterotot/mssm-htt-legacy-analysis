e29f7dd797e1861b6ee798454512447c5a5da127
Maximilian Burkart <maxiburkart@gmail.com>
2021-01-07 16:08:50 +0100

    Update remaining trigger selections in 2016 and 2018.

--- config/shapes/channel_selection.py
+++ config/shapes/channel_selection.py
@@ -20,18 +20,24 @@ def channel_selection(channel, era):
         #  Add era specific cuts. This is basically restricted to trigger selections.
         if era == "2016":
             cuts.append(
-                ("pt_2>30 && ((pt_1 >= 23 && trg_singlemuon == 1) || (trg_mutaucross == 1 && pt_1 < 23 && abs(eta_2)<2.1))","trg_selection")
+                ("pt_2>30"
+                 "&& (((isSingleMuon||isMC||isEmbedded) && ((pt_2<120 && fabs(eta_2)<2.1 && pt_1 < 23 &&  (trg_mutaucross==1)) || (pt_1 >= 23 && (trg_singlemuon==1))))"
+                     "|| ((isMC||isEmbedded) && ((trg_singletau120_trailing==1) || (trg_singletau140_trailing==1)) && pt_2>=120 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1 >= 23 && (trg_singlemuon==1)) && ((trg_singletau120_trailing==1) || (trg_singletau140_trailing==1)) && pt_2>=120 && fabs(eta_2)<2.1)", "trg_selection")
             )
         elif era == "2017":
             cuts.append(
                 ("pt_2>30"
                  "&& (((isSingleMuon||isMC||isEmbedded) && ((pt_2>32 && pt_2<180 && fabs(eta_2)<2.1 && pt_1<25 && (trg_crossmuon_mu20tau27==1)) || (pt_1>=25 && ((trg_singlemuon_27==1) || (trg_singlemuon_24==1)))))"
-                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>180 && fabs(eta_2)<2.1)"
-                     "|| ((isTau) && !(pt_1>=25 && ((trg_singlemuon_27==1) || (trg_singlemuon_24==1))) && (trg_singletau_trailing==1) && pt_2>180 && fabs(eta_2)<2.1))", "trg_selection"),
+                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1>=25 && ((trg_singlemuon_27==1) || (trg_singlemuon_24==1))) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1))", "trg_selection"),
             )
         elif era == "2018":
             cuts.append(
-                ("pt_2>30 && ((trg_singlemuon_27 == 1) || (trg_singlemuon_24 == 1)) || (pt_1 < 25 && (trg_crossmuon_mu20tau27_hps == 1 || trg_crossmuon_mu20tau27 == 1))", "trg_selection"),
+                ("pt_2>30"
+                 "&& (((isSingleMuon||isMC||isEmbedded) && ((pt_2>32 && pt_2<180 && fabs(eta_2)<2.1 && pt_1<25 && (trg_crossmuon_mu20tau27_hps == 1 || trg_crossmuon_mu20tau27 == 1)) || (pt_1>=25 && ((trg_singlemuon_27 == 1) || (trg_singlemuon_24 == 1)))))"
+                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1>=25 && ((trg_singlemuon_27 == 1) || (trg_singlemuon_24 == 1))) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1))", "trg_selection"),
             )
         else:
             raise ValueError("Given era does not exist")
@@ -46,18 +52,24 @@ def channel_selection(channel, era):
         ])
         if era == "2016":
             cuts.append(
-                ("pt_2>30 && ((pt_1>26 && (trg_singleelectron==1)) || (pt_1<26 && pt_1>25 && (trg_eletaucross==1)))", "trg_selection"),
+                ("pt_2>30"
+                 "&& (((isSingleElectron||isMC||isEmbedded) && ((pt_2<120 && fabs(eta_2)<2.1 && pt_1<26 && pt_1>25 && (trg_eletaucross==1)) || (pt_1>=26 && (trg_singleelectron==1))))"
+                     "|| ((isMC||isEmbedded) && ((trg_singletau120_trailing==1) || (trg_singletau140_trailing==1)) && pt_2>=120 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1>=26 && (trg_singleelectron==1)) && ((trg_singletau120_trailing==1) || (trg_singletau140_trailing==1)) && pt_2>=120 && fabs(eta_2)<2.1))", "trg_selection"),
             )
         elif era == "2017":
             cuts.append(
                 ("pt_2>30"
                  "&& (((isSingleElectron||isMC||isEmbedded) && ((pt_2>35 && pt_2<180 && fabs(eta_2)<2.1 && pt_1<28 && (trg_crossele_ele24tau30==1)) || (pt_1>=28 && ((trg_singleelectron_27==1) || (trg_singleelectron_32==1) || (trg_singleelectron_35==1)))))"
-                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>180 && fabs(eta_2)<2.1)"
-                     "|| ((isTau) && !(pt_1>=28 && ((trg_singleelectron_27==1) || (trg_singleelectron_32==1) || (trg_singleelectron_35==1))) && (trg_singletau_trailing==1) && pt_2>180 && fabs(eta_2)<2.1))", "trg_selection"),
+                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1>=28 && ((trg_singleelectron_27==1) || (trg_singleelectron_32==1) || (trg_singleelectron_35==1))) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1))", "trg_selection"),
             )
         elif era == "2018":
             cuts.append(
-                ("pt_2>30 && ((trg_singleelectron_35 == 1) || (trg_singleelectron_32 == 1) || (pt_1>25 && pt_1<33 && pt_2>35 && (trg_crossele_ele24tau30_hps == 1 || trg_crossele_ele24tau30 == 1)))", "trg_selection"),
+                ("pt_2>30"
+                 "&& (((isSingleElectron||isMC||isEmbedded) && ((pt_2>35 && pt_2<180 && fabs(eta_2)<2.1 && pt_1>25 && pt_1<33 && (trg_crossele_ele24tau30_hps==1 || trg_crossele_ele24tau30==1)) || (pt_1 >=33 && ((trg_singleelectron_35==1) || (trg_singleelectron_32==1)))))"
+                     "|| ((isMC||isEmbedded) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1)"
+                     "|| ((isTau) && !(pt_1 >=33 && ((trg_singleelectron_35==1) || (trg_singleelectron_32==1))) && (trg_singletau_trailing==1) && pt_2>=180 && fabs(eta_2)<2.1))", "trg_selection"),
             )
         else:
             raise ValueError("Given era does not exist")
@@ -71,7 +83,7 @@ def channel_selection(channel, era):
         ])
         if era == "2016":
             cuts.append(
-                ("trg_doubletau==1", "trg_doubletau"),
+                    ("(trg_doubletau==1) || ((pt_1>120)&&((trg_singletau120_leading==1)||(trg_singletau140_leading==1))) || ((pt_2>120)&&((trg_singletau120_trailing==1)||(trg_singletau140_trailing==1)))", "trg_doubletau"),
             )
         elif era == "2017":
             cuts.append(
@@ -79,7 +91,7 @@ def channel_selection(channel, era):
             )
         elif era == "2018":
             cuts.append(
-                ("(((!(isMC||isEmbedded) && run>=317509) || (isMC||isEmbedded)) && (trg_doubletau_35_mediso_hps == 1)) || (!(isMC||isEmbedded) && (run<317509) && ((trg_doubletau_35_tightiso_tightid == 1) || (trg_doubletau_40_mediso_tightid == 1) || (trg_doubletau_40_tightiso == 1)))", "trg_selection"),
+                ("((((!(isMC||isEmbedded) && run>=317509) || (isMC||isEmbedded)) && (trg_doubletau_35_mediso_hps==1)) || (!(isMC||isEmbedded) && (run<317509) && ((trg_doubletau_35_tightiso_tightid==1) || (trg_doubletau_40_mediso_tightid==1) || (trg_doubletau_40_tightiso==1)))) || ((pt_1>180)&&((trg_singletau_leading==1))) || ((pt_2>180)&&(trg_singletau_trailing==1))", "trg_selection"),
             )
         else:
                 raise ValueError("Given era does not exist")

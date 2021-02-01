bf9d41b4905998d7f418db9a904dfa8ff30c0a13
Lucas Torterotot <lucas.torterotot@gmail.com>
2021-02-01 11:51:49 +0100

    Update scripts

--- plotting/plot_shapes_control.sh
+++ plotting/plot_shapes_control.sh
@@ -3,15 +3,14 @@
 
 ERA=$1
 INPUT=$2
+ch=$3
 
 v="pt_1,pt_2,eta_1,eta_2,m_vis,m_sv_puppi,pt_tt_puppi,ptvis,jpt_1,jpt_2,jeta_1,jeta_2,bpt_1,bpt_2,puppimet,DiTauDeltaR,pZetaPuppiMissVis,mt_1_puppi,mt_2_puppi,mTdileptonMET_puppi,njets,nbtag,jdeta,dijetpt,mjj"
-for ch in "mt" "et" "tt" "em"
-do
-    plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --embedding --fake-factor
-    plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --fake-factor
-    plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --embedding
-    plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch}
-done
+
+plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --embedding --fake-factor --normalize-by-bin-width
+plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --fake-factor --normalize-by-bin-width
+plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --embedding --normalize-by-bin-width
+plotting/plot_shapes_control.py -l --era Run${ERA} --input $INPUT --variables ${v} --channels ${ch} --normalize-by-bin-width
 
 #v="NNrecoil_pt,nnmet,mt_1_nn,mt_2_nn,mt_tot_nn,pt_tt_nn,pZetaNNMissVis,pt_ttjj_nn,mTdileptonMET_nn"
 #ch="mt,et,tt,em"

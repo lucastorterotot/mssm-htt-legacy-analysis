cacb0a2ba9165ce55755aab13098e4a8cfdecccb
Maximilian Burkart <maxiburkart@gmail.com>
2021-02-09 11:01:19 +0100

    Update top level scripts for control plots.

--- make_control_plots.sh
+++ make_control_plots.sh
@@ -11,10 +11,23 @@ else
     VARIABLES=(pt_1 pt_2 eta_1 eta_2 m_sv_puppi m_vis jpt_1 jpt_2 jeta_1 jeta_2 dijetpt jdeta mjj bpt_1 bpt_2 mTdileptonMET_puppi mt_1_puppi mt_2_puppi pZetaPuppiMissVis ptvis pt_tt_puppi puppimet)
 fi
 
+# Merge the shapes.
+# bash submit/merge_outputs.sh $ERA $CHANNEL $TAG 1
+
+# Produce the different estimations.
+bash shapes/do_estimations.sh $ERA output/shapes/${ERA}-${CHANNEL}-control-shapes-${TAG}/shapes-control-${ERA}-${CHANNEL}.root 1
+
+# Convert them to synced shapes,
+bash shapes/convert_to_synced_shapes.sh $ERA $CHANNEL dummy $TAG 1
+
 for VAR in ${VARIABLES[@]}
 do
+    # Produce the datacards.
     bash datacards/produce_gof_datacard.sh $ERA $CHANNEL $VAR $TAG
+    # Produce the workspaces.
     bash datacards/produce_gof_workspace.sh $ERA $CHANNEL $VAR
+    # Produce the prefit shapes.
     bash shapes/extract_postfit_shapes.sh $ERA $CHANNEL $VAR
-    bash plotting/plot_shapes_gof.sh $ERA $CHANNEL $VAR 1 1
+    # Produce the plots.
+    bash plotting/plot_shapes_control_systematics.sh $ERA $CHANNEL $VAR 1 1
 done

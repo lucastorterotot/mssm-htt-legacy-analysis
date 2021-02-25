c5ce766f943caa7dd538bc060d5a6f5a2af158b5
Lucas Torterotot <lucas.torterotot@gmail.com>
2021-02-25 09:27:31 +0100

    Update y ratio range

--- plotting/plot_shapes_mssm.py
+++ plotting/plot_shapes_mssm.py
@@ -401,15 +401,16 @@ def main(args):
                 
             width=.3
             plot.subplot(2).setYlims(1-width, 1+width)#(0.75, 1.8)
-            if channel in ["et", "mt"] and int(category) in [32, 35]:
-                if 1+width < 2:
-                    plot.subplot(2).setYlims(1-width, 2)
-            elif channel == "tt" and int(category) in [32]:
-                if 1+width < 2:
-                    plot.subplot(2).setYlims(1-width, 2)
-            elif channel == "tt" and int(category) in [35]:
-                if 1+width < 3:
-                    plot.subplot(2).setYlims(1-width, 3)
+            if args.model_independent:
+                if channel in ["et", "mt"] and int(category) in [32, 35]:
+                    if 1+width < 2:
+                        plot.subplot(2).setYlims(1-width, 2)
+                elif channel == "tt" and int(category) in [32]:
+                    if 1+width < 2:
+                        plot.subplot(2).setYlims(1-width, 2)
+                elif channel == "tt" and int(category) in [35]:
+                    if 1+width < 3:
+                        plot.subplot(2).setYlims(1-width, 3)
 
             if args.linear and args.split:
                 # plot.subplot(1).setYlims(1.e-4, split_dict[channel])
